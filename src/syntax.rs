ef84ea279db11f557f7759821315c65b6812b5ce
Caleb Riley <86499888+caleb-riley@users.noreply.github.com>
2025-04-11 23:25:28 -0400

    Syntax elements no longer need to be clonable

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,18 +1,15 @@
 use crate::value::Operator;
 
-#[derive(Clone)]
 pub enum Decl {
     FnDecl(FnDecl),
 }
 
-#[derive(Clone)]
 pub struct FnDecl {
     pub name: String,
     pub params: Vec<String>,
     pub body: Vec<Stmt>,
 }
 
-#[derive(Clone)]
 pub enum Stmt {
     FnCall(FnCall),
     Return(ReturnStmt),
@@ -21,30 +18,25 @@ pub enum Stmt {
     Decl(DeclStmt),
 }
 
-#[derive(Clone)]
 pub struct AssignStmt {
     pub var: String,
     pub val: Expr,
 }
 
-#[derive(Clone)]
 pub struct DeclStmt {
     pub var: String,
     pub val: Expr,
 }
 
-#[derive(Clone)]
 pub struct ReturnStmt {
     pub expr: Expr,
 }
 
-#[derive(Clone)]
 pub struct IfStmt {
     pub cond: Expr,
     pub body: Vec<Stmt>,
 }
 
-#[derive(Clone)]
 pub enum Expr {
     Identfier(String),
     NumberLiteral(i32),
@@ -58,26 +50,22 @@ pub enum Expr {
     Unary(Unary),
 }
 
-#[derive(Clone)]
 pub struct FieldAccess {
     pub obj: Box<Expr>,
     pub field: String,
 }
 
-#[derive(Clone)]
 pub struct Binary {
     pub op: Operator,
     pub left: Box<Expr>,
     pub right: Box<Expr>,
 }
 
-#[derive(Clone)]
 pub struct Unary {
     pub op: Operator,
     pub expr: Box<Expr>,
 }
 
-#[derive(Clone)]
 pub struct FnCall {
     pub name: String,
     pub args: Vec<Expr>,
