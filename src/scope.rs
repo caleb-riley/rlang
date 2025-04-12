58958a4037121b6f8ad9d054c0783deeee9ccb5c
caleb-riley <86499888+caleb-riley@users.noreply.github.com>
2025-04-12 15:31:55 -0400

    Documented ScopeManager and Scope

--- src/scope.rs
+++ src/scope.rs
@@ -2,21 +2,22 @@ use std::collections::HashMap;
 
 use crate::value::Value;
 
-pub struct ScopeHolder {
+/// Holds a scope to allow push and pop operations.
+#[derive(Default)]
+pub struct ScopeManager {
+    /// The optional current scope, if it exists.
     scope: Option<Scope>,
 }
 
-impl ScopeHolder {
-    pub fn new() -> Self {
-        Self { scope: None }
-    }
-
+impl ScopeManager {
+    /// Adds a new level to the scope hierarchy.
     pub fn push_scope(&mut self) {
         let current_scope = self.scope.take();
 
         self.scope = Some(Scope::new(current_scope));
     }
 
+    /// Removes the last level from the scope hierarchy.
     pub fn pop_scope(&mut self) -> Result<(), ()> {
         let current_scope = self.scope.take().ok_or(())?;
 
@@ -25,21 +26,28 @@ impl ScopeHolder {
         Ok(())
     }
 
+    /// Returns a shared reference to the inner scope.
     pub fn inner(&self) -> Option<&Scope> {
         self.scope.as_ref()
     }
 
+    // Returns an exclusive reference to the inner scope.
     pub fn inner_mut(&mut self) -> Option<&mut Scope> {
         self.scope.as_mut()
     }
 }
 
+/// Holds the variables present at the current level of execution.
 pub struct Scope {
-    pub variables: HashMap<String, Value>,
-    pub parent: Option<Box<Scope>>,
+    /// A hash map storing the variables as key value pairs.
+    variables: HashMap<String, Value>,
+
+    /// The optional parent of this scope, inherits values from it.
+    parent: Option<Box<Scope>>,
 }
 
 impl Scope {
+    /// Creates a new scope, given the optional parent scope.
     fn new(parent: Option<Self>) -> Self {
         Self {
             variables: HashMap::new(),
@@ -47,33 +55,40 @@ impl Scope {
         }
     }
 
-    pub fn get(&self, name: &str) -> Option<&Value> {
-        if let var @ Some(_) = self.variables.get(name) {
+    /// Returns a shared reference to the value with the given name.
+    pub fn get(&self, var_name: &str) -> Option<&Value> {
+        if let var @ Some(_) = self.variables.get(var_name) {
             return var;
         }
 
         match self.parent {
-            Some(ref parent) => parent.get(name),
+            Some(ref parent) => parent.get(var_name),
             None => None,
         }
     }
 
-    fn get_mut(&mut self, name: &str) -> Option<&mut Value> {
-        if let var @ Some(_) = self.variables.get_mut(name) {
-            return var;
+    /// Returns an exclusive reference to the value with the given name.
+    fn get_mut(&mut self, var_name: &str) -> Option<&mut Value> {
+        if let value @ Some(_) = self.variables.get_mut(var_name) {
+            return value;
         }
 
         match self.parent {
-            Some(ref mut parent) => parent.get_mut(name),
+            Some(ref mut parent) => parent.get_mut(var_name),
             None => None,
         }
     }
 
-    pub fn set(&mut self, name: &str, obj: Value) -> Result<(), ()> {
-        self.get_mut(name).map(|v| *v = obj).ok_or(())
+    /// Sets the value of the variable with the given name to the given value.
+    pub fn set(&mut self, var_name: &str, new_value: Value) -> Result<(), ()> {
+        self.get_mut(var_name)
+            .map(|value| *value = new_value)
+            .ok_or(())
     }
 
-    pub fn declare(&mut self, name: String, value: Value) {
-        self.variables.insert(name, value);
+    /// Declares a new variable with the given name in the current scope, giving
+    /// it the initial provided value.
+    pub fn declare(&mut self, var_name: String, initial_value: Value) {
+        self.variables.insert(var_name, initial_value);
     }
 }
